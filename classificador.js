0cffa8c28353a7900c4273f85e430ba62ddc2ec9
a-1219 <absilva@inventati.org>
2024-06-12 05:45:03 -0300

    criação do classificador

---
+++ classificador.js
@@ -0,0 +1,23 @@
+let nome = "Mansa Musa"
+let xp = Math.floor(Math.random()*10001)
+let nvl = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"]
+
+if (xp <= 1000) {
+    nvl = nvl[0]
+} else if (xp > 1000 && xp <= 2000) {
+    nvl = nvl[1]
+} else if (xp > 2000 && xp <= 5000) {
+    nvl = nvl[2]
+} else if (xp > 5000 && xp <= 7000) {
+    nvl = nvl[3]
+} else if (xp > 7000 && xp <= 8000) {
+    nvl = nvl[4]
+} else if (xp > 8000 && xp <= 9000) {
+     nvl = nvl[5]
+} else if (xp > 9000 && xp <= 10000) {
+    nvl = nvl[6]
+} else if (xp >= 10000) {
+    nvl = nvl[7]
+}
+
+console.log(`O herói ${nome} está no nível ${nvl}`)
